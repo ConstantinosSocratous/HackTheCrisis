f8318c8f240a40e5dcc755432aa545b4f1013203
ConstantinosSocratous <constantinos.socratous@cytanet.com.cy>
2020-04-04 18:56:29 +0300

    finish book apt page

--- Clickadoc/src/app/services/user.service.ts
+++ Clickadoc/src/app/services/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   pastActivity: Array<any> = [
     { doctor: 'John', date: '03/04/2020', notes: 'Notes for the 1st meeting, bla bla bla'}, 
     { doctor: 'John', date: '22/03/2020', notes: 'These are the notes for the 2nd meeting'}, 
-    { doctor: 'John', date: '13/03/2020', notes: 'bla bla bla bla bla blabla bla blabla bla bla'}, 
+    { doctor: 'John', date: '13/03/2020', notes: 'bla bla bla bla bla blabla bla blabla bla bla a bladsfdsfdsf a bladsfdsfdsf a bladsfdsfdsf'}, 
     { doctor: 'John', date: '27/02/2020', notes: 'bla bla blabla bla blabla bla bla dsdfdsf'}, 
     { doctor: 'John', date: '02/02/2020', notes: 'bla bla blabla bla blabla bla bladsfdsfdsfdsfdsf'}, 
   ]
--- Clickadoc/src/app/tabPages/tab-page2/tab-page2.component.ts
+++ Clickadoc/src/app/tabPages/tab-page2/tab-page2.component.ts
@@ -9,9 +9,12 @@ import { AlertController, ToastController } from '@ionic/angular';
 export class TabPage2Component implements OnInit {
 
   reason: String = ''
-  date = "2020-04-04"
-  time = "2020-04-04T07:30Z"
-  requestedAppointments: Array<any> = []
+  date:Date// "2020-04-04"
+  time:Date// = "2020-04-04T07:30Z"
+  requestedAppointments: Array<any> = [
+    {reason: 'Emergency', date: '7-4-2020', time: '09:00', status: "Waiting for approval"},
+    {reason: 'Usual checkup', date: '12-4-2020', time: '10:30', status: "Scheduled"}
+  ]
 
   constructor(private alertController: AlertController,
     private toastController: ToastController) { }
@@ -32,13 +35,13 @@ export class TabPage2Component implements OnInit {
           role: 'cancel',
           cssClass: 'secondary',
           handler: (blah) => {
-            console.log('Confirm Cancel: blah');
+            // console.log('Confirm Cancel: blah');
             this.presentToast('Cacnel', 1500)
           }
         }, {
           text: 'Okay',
           handler: () => {
-            console.log('Confirm Okay');
+            // console.log('Confirm Okay');
             this.presentToast('Request sent successfully',2500)
             this.addRequest()
           }
@@ -59,8 +62,17 @@ export class TabPage2Component implements OnInit {
 
   addRequest(){
     let tm = new Date(this.time)
-    // console.log(this.time, tm)
-    this.requestedAppointments.push({reason: this.reason, date: this.date, time: tm.getHours() + ":" + tm.getMinutes()})
+    let dt = new Date(this.date)
+    this.requestedAppointments.push({reason: this.reason, date: dt.getDate() + "-" +
+    (dt.getMonth()+1) + "-" + dt.getFullYear(), time: tm.getHours() + ":" + tm.getMinutes(), status: "Waiting for approval"})
     // console.log(this.requestedAppointments.length)
   }
+
+  cancel(index){
+    let object = this.requestedAppointments[index] // Item to remove
+    this.requestedAppointments = this.requestedAppointments.filter(obj => obj !== object);
+    // console.log(this.requestedAppointments)
+    // delete this.requestedAppointments[index]
+    // console.log(this.requestedAppointments)
+  } 
 }
